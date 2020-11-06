416a0d93fd5dc596198e09b534009831e12d5eb3
Dohyeong <panamera1594@gmail.com>
2020-11-06 15:00:33 +0900

    node 최신버전 esm환경 지원하기 위해 dual package로 구성

---
+++ babel.config.cjs
@@ -0,0 +1,17 @@
+module.exports = (api) => {
+  const BABEL_ENV = api.env();
+  const targets = { node: BABEL_ENV === "cjs" ? 6 : 12 };
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          targets,
+          useBuiltIns: "usage",
+          corejs: 3,
+          modules: BABEL_ENV === "mjs" ? false : "auto",
+        },
+      ],
+    ],
+  };
+};
