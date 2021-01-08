c05d8cdea990e679af92ab3d7ab2a99be875d13b
Dohyeong <panamera1594@gmail.com>
2021-01-08 17:26:55 +0900

    babel transform-runtime으로 transpile하도록 수정

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,17 +1,24 @@
 module.exports = (api) => {
   const BABEL_ENV = api.env();
   const targets = { node: BABEL_ENV === "cjs" ? 6 : 12 };
+  const plugins = [
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        corejs: 3,
+      },
+    ],
+  ];
   return {
     presets: [
       [
         "@babel/preset-env",
         {
           targets,
-          useBuiltIns: "usage",
-          corejs: '3.8',
           modules: BABEL_ENV === "mjs" ? false : "auto",
         },
       ],
     ],
+    plugins,
   };
 };
